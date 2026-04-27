6d5ef33c0a781eb25f672e4c7daac090572e2af9
Saif Ullah <saifu0905@gmail.com>
2026-04-27 10:14:07 +0500

    created state.py and assistant_graph.py which is the orchestrator of the project, in short builder of the agent/graph

---
+++ assistant_graph.py
@@ -0,0 +1,31 @@
+from langchain_core.messages import SystemMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.prebuilt import ToolNode, tools_condition
+from langgraph.graph import StateGraph
+from langgraph.checkpoint.memory import InMemorySaver
+from state import AgentState
+
+class Agent:
+    def __init__(self, tools, name="Luna", model="gemini-2.5-flash-preview-native-audio-dialog"):
+        self.system_prompt = "You are Luna, the company's expense manager..."
+        self.tools = tools
+        self.llm = ChatGoogleGenerativeAI(model=model).bind_tools(tools=self.tools)
+        self.graph = self.build_graph()
+
+    def build_graph(self):
+        builder = StateGraph(AgentState)
+
+        def assistant(state: AgentState):
+            system_prompt = self.system_prompt.format(customer_id=state.customer_id)
+            response = self.llm.invoke([SystemMessage(content=system_prompt)] + state.messages)
+            state.messages.append(response)
+            return state
+
+        builder.add_node(assistant)
+        builder.add_node(ToolNode(self.tools))
+        
+        builder.set_entry_point("assistant")
+        builder.add_conditional_edges("assistant", tools_condition)
+        builder.add_edge("tools", "assistant")
+
+        return builder.compile(checkpointer=InMemorySaver())
